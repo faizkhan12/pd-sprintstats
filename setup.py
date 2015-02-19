993f7e7fd1a1b9064c9b133dc1124673bd5d0e08
Jason Diller <jdiller@pagerduty.com>
2015-02-19 15:36:08 -0500

    Make the script distributable as a python wheel

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+from distutils.core import setup
+import setuptools
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setup(name='pd-sprintstats',
+      version='1.0',
+      description='Gathers some statistics for a sprint from JIRA',
+      author='Jason Diller',
+      author_email='jdiller@pagerduty.com',
+      url='',
+      install_requires=required,
+      scripts=['sprintstats'],
+)
